6b9fb9c41b4063b472bd40cabef2da96b1c16c8c
gilescm <34382610+gilescm@users.noreply.github.com>
2019-02-07 20:41:02 +0000

    Removed AddGlider
    - This was only used for testing purposes, gliders has a fairly high chance of being created when using `randomise` as the seed.

--- State.py
+++ State.py
@@ -17,28 +17,18 @@ class State:
 
         if Seed == 'random':
             self.Randomise()
-        elif Seed == '5':
-            self.Scenario5()
-        elif Seed == 'glider':
-            self.AddGlider()
-
+        elif Seed == 'blinker':
+            self.Blinker()
 
     # Returns a grid with randomised nxn cells
     def Randomise(self): 
         self.grid = Num.random.choice(self.Values, self.length*self.length, p=[0.15, 0.85]).reshape(self.length, self.length) 
 
     # Returns a grid equal to the initial state in scenario 5 on the test brief
-    def Scenario5(self): 
+    def Blinker(self): 
         self.grid[(self.length//2), (self.length//2)+1] = ON
         self.grid[(self.length//2),     self.length//2] = ON
         self.grid[(self.length//2), (self.length//2)-1] = ON
-
-    def AddGlider(self):
-        """adds a glider with top left cell at (i, j)"""
-        glider = Num.array([[OFF, OFF, ON],  
-                           [ON , OFF, ON],  
-                           [OFF,  ON, ON]]) 
-        self.grid[0:3, 0:3] = glider 
 
     # Returns true if we need to extend the board grid due to live cells at the edge
     def EdgeCase(self):
